775c75f00ed0427b565762f042913a49e84a3273
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2022-11-08 11:15:47 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='otello',
-    version='1.0.1',
+    version='1.0.2',
     long_description='Wrapper tool for notebook users to communicate with HySDS components',
     packages=find_packages(),
     include_package_data=True,
